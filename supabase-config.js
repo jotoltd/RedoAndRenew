071241169cf0da5c99d99b70f0b3252c5ed7ad30
Digital Gaff <digitalgaff@users.noreply.github.com>
2026-09-19 18:13:19 +0100

    Fix Safari global collision on supabase client variable

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@
 const SUPABASE_URL = "https://jbicfyqhyictnxafzyav.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpiaWNmeXFoeWljdG54YWZ6eWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1OTgwODEsImV4cCI6MjEwNTE3NDA4MX0.9ymQImK1lOzu8-4ILvxl3nSMHDqnvl7eLfTpb3I0DwQ";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+var supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 /* Admin login credentials — change these to whatever you want */
 const ADMIN_EMAIL = "charline@redoandrenew.co.uk";
